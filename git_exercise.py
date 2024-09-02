f745a39c33ae8eaf8410a1249f8224047f1f099e
biankasledz <167304931+biankasledz@users.noreply.github.com>
2024-09-02 12:36:58 +0200

    Update git_exercise.py

--- git_exercise.py
+++ git_exercise.py
@@ -2,3 +2,6 @@ import hola.py
 import bib.py
 def team_introduction():
     print("This is Team Feminionioin. We are:")
+    print(hola.get_name())
+    print(bib.get_name())
+    print(member3.get_name())
