5c37930c400b69c0954d82f11fb0f0eff1e93b7d
arament <maja.arament@student.uva.nl>
2024-09-03 19:50:08 +0200

    station4.py

---
+++ challenge_day2/station4.py
@@ -0,0 +1,11 @@
+def solution_station_4(n):
+    if n == 1:
+        return False
+    elif n > 1:
+        for i in range(2,n):
+            if (n % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
