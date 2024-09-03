9945dbf30cb72d617e23fd60451d9f3834e3912a
arament <maja.arament@student.uva.nl>
2024-09-03 20:57:00 +0200

    station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,20 +1,17 @@
-def find_lt_group(name):
-    lt_groups = {
+import pandas as pd
+
+def solution_station_5(name):
+    teamsnames= {
         5: ["Aya", "Shriya", "Lumi", "Anouk", "Walter", "Joeben", "Maria", "Davide", "Leon", "Calson", "Ilias", "Sophia", "Anna", "Markus", "Lucas", "Thaiss", "Fleming", "Wu", "Bey", "Emilie", "Bianka"],
         1: ["Bissola", "Ava", "Rasmus", "Christal", "Catherin", "Philine", "Farah", "Mirre", "Adam", "Scarlett", "Maximilian", "Rafael", "Josy", "Meng", "Collins", "David", "Weizhao", "Anna", "Nojus", "Porter", "Maria", "Nia"],
         2: ["Sean", "Maja", "Emma-Rose", "Irene", "Ana", "Alexandru", "Jacco", "Ömer", "Kimya", "Andre", "Jamie", "Yujia", "Koyo", "Viggo", "In", "Derrick", "Juliette", "Lucia", "Melody", "Federico"],
         3: ["Gadis", "Gabriella", "Alejandro", "Roy", "Nikko", "Wentao", "Felicia", "Micay", "Elnara", "Mana", "Khushi", "Sina", "Tadas", "Daniel", "Alexia", "Alexandra", "Viktor", "Clement", "Sihun", "Merijn", "Mark"],
-        4: ["Dorottya", "Lotte", "Volodymyr", "Alva", "Brendan", "Callum", "Angela", "Regina", "Danielius", "Akira", "David", "Noel", "Gabriela", "Marwan", "Kazuya", "Callum", "Danielius", "Dasha", "Cem", "Asjfaaq", "Khushi", "Elpiniki", "Shuting", "Antonin", "Antonio", "Niki"]
-    }
-    
-    for lt_number, names in lt_groups.items():
-        if name in names:
-            return f"{name} is in LT{lt_number}"
-    
-    return f"{name} is not in any LT group"
+        4: ["Dorottya", "Lotte", "Volodymyr", "Alva", "Brendan", "Callum", "Angela", "Regina", "Danielius", "Akira", "David", "Noel", "Gabriela", "Marwan", "Kazuya", "Callum", "Danielius", "Dasha", "Cem", "Asjfaaq", "Khushi", "Elpiniki", "Shuting", "Antonin", "Antonio", "Niki"]}
 
-# Example usage:
-print(find_lt_group("Aya"))  # Output: Aya is in LT5
-print(find_lt_group("Markus"))  # Output: Markus is in LT5
-print(find_lt_group("Rafael"))  # Output: Rafael is in LT1
-print(find_lt_group("John"))  # Output: John is not in any LT group
+    df = pd.read_excel(teamnames)
+    
+    try:
+        team = df.loc[df['Student firstname'] == name, 'Team'].values[0]
+        return int(team)
+    except IndexError:
+        return "This person is not in the course"
