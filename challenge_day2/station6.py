7c8c03e6d09cf57700345baa46fd3bc925cbc2f7
biankasledz <167304931+biankasledz@users.noreply.github.com>
2024-09-03 09:49:14 +0200

    Update station6.py

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -1,5 +1,11 @@
 solution_station_6(print)
-sample_input=={1.6, 0.8, 1.9, 0.3, 1.3, 2.8, 2.6}
+sample_input={1.6, 0.8, 1.9, 0.3, 1.3, 2.8, 2.6}
 input={38, 58, 22, 30, 72, 73, 15, 46}
 sample_output={0.9996, 0.7174, 0.9463, 0.2955, 0.9636, 0.335, 0.5155}
-output
+def output:
+  (input * sample_output)/sample_output
+
+
+solution_station_6
+station6_input: int,
+observation6
