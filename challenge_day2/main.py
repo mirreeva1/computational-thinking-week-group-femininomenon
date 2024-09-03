76b58a9c7011b3b12a9a2d550b2baf75f50f30fa
arament <maja.arament@student.uva.nl>
2024-09-03 13:45:03 +0200

    Update main.py

--- challenge_day2/main.py
+++ challenge_day2/main.py
@@ -4,21 +4,19 @@ from station3 import solution_station_3
 from station5 import solution_station_5
 from station6 import solution_station_6
 
-observation2 = 
-observation3 = 
-observation5 = 
-observation6 = ('18:15:00', 'input6', 8, 'data8', 3.14)
+observation2 = ('10:09:20', '2024-07-15', 3235, "Kimiya", 12)
+observation3 = ('10:09:34', '2024-01-14', 52762, "Tadas", 52)
+observation5 = ('10:09:41', '2024-11-08',8674, "Mark", 86
 
 def combined_algorithm(observations: tuple) -> int:
-    output2 = solution_station_2(observations[1])
+    output2 = solution_station_2(observations[5])
     output3 = solution_station_3(observations[2])
     output5 = solution_station_5(observations[3])
-    output6 = solution_station_6(observations[4])
     
     assert isinstance(output2, str)
     assert isinstance(output3, bool)
     assert isinstance(output5, int)
-    assert isinstance(output6, float)
+    
     
     return int.from_bytes(output2[0].encode("unicode_escape"), byteorder='big') * (3 if output3 else 2) * output5 * output6
 
