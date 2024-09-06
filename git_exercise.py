c4ac8c0a864212e7db2afa87a4eb729077e99e23
arament <maja.arament@student.uva.nl>
2024-09-06 09:17:39 +0200

    Update git_exercise.py

--- git_exercise.py
+++ git_exercise.py
@@ -3,8 +3,6 @@ import Angela
 import bib
 import me
 
-c1, c2, c3, c4, c5 = maja.character(), martyna.character(), Vincent.character(), eryk.character(), mate.character()
-
 def funct():
     print(f"This is group Femininomenon. We are {hola.get_name()}, {Angela.string_name()}, {bib.name()}.\n")
 funct()
