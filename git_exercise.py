eb0780dd53a49470fa03d2968c17d4e63b912a43
arament <maja.arament@student.uva.nl>
2024-09-06 09:40:52 +0200

    Update git_exercise.py

--- git_exercise.py
+++ git_exercise.py
@@ -5,5 +5,5 @@ import me
 import mirre
 
 def funct():
-    print(f"This is group Femininomenon. We are {hola.get_name()}, {mirre.get_name()}, {me.get_name()}, {Angela.string_name()}, {bib.name()}.\n")
+    print(f"This is group Femininomenon. We are {hola.get_name()}, {mirre.get_name()}, {me.get_name()}, {Angela.get_name()}, {bib.get_name()}.\n")
 funct()
